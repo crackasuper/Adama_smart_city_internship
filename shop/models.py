2335d33eb54d75d5530205c990d54f086bb247a1
crackasuper <crackabad859@gmail.com>
2024-08-03 13:17:17 +0300

    creating product and dashboard model

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,3 +9,22 @@ class stores(models.Model):
     product = models.CharField(max_length=100)
     quantity = models.BigIntegerField()
     price = models.CharField(max_length=100)
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_description = models.CharField(max_length=500)
+    category = models.CharField(max_length=100)
+    price = models.FloatField()
+
+class Dashboard(models.Model):
+    total_sales = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_products = models.IntegerField(default=0)
+    total_revenue = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_customers = models.IntegerField(default=0)
+    
+    top_selling_products = models.CharField(max_length=255, blank=True)
+    date = models.DateField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f'Dashboard for {self.date}'
+    
