c043321dc8f15358d41117bea2be84287f7fdd9d
crackasuper <crackabad859@gmail.com>
2024-08-07 12:19:58 +0300

    configuring urls

--- core/urls.py
+++ core/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from .views import SignUpView
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', SignUpView.as_view(), name= 'signup'),
+]
