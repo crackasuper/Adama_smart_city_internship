c50a92ab118a04fed09bd072b9e3b00c6c01e6bb
crackasuper <crackabad859@gmail.com>
2024-08-07 09:08:47 +0300

    updating and adding additional models

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
@@ -14,13 +15,47 @@ class stores(models.Model):
         return f'{self.product}'
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=100)
-    product_description = models.CharField(max_length=500)
-    category = models.CharField(max_length=100)
-    price = models.FloatField()
+    product_name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add= True)
+    modified_date = models.DateTimeField(auto_now= True)
+    image = models.ImageField(upload_to='products/')
+
+   
 
     def __str__(self) -> str:
         return f'{self.product_name}'
+    def get_url(self):
+        return reverse('product_detail', args=[self.slug])
+    
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager,self).filter(variation_category = "color", is_active = True)
+    def sizes(self):
+        return super(VariationManager,self).filter(variation_category = "size", is_active = True)
+variation_category_choice=(
+    ('color','color'),
+    ('size','size')
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.variation_value
+    objects = VariationManager()
+
+
+
 
 class Dashboard(models.Model):
     total_sales = models.DecimalField(max_digits=10, decimal_places=2, default=0)
