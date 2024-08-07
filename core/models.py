55b7604df1c4010b9734a9d8ddc0a74c2c824b4f
crackasuper <crackabad859@gmail.com>
2024-08-07 10:16:20 +0300

    preparing category model and registering on admin site

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category_image')
+
+    class Meta:
+        verbos_name = "Category"
+        verbos_name_plural = "Categories"
+    def get_url(self):
+        return reverse('product_by_category', args=[self.slug])
+    def __str__(self) -> str:
+        return self.category_name
